5da4ef2b60003197b9f6a351debddc487ea079b1
Bulat <zaripovbi@gmail.com>
2016-04-26 08:51:07 +0300

    Добавьте файлы проекта.

---
+++ Sorts/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sorts
+{
+    class Program
+    {
+        const int MAXN = 100;
+        const int RANDMIN = 0;
+        const int RANDMAX = 1000;
+
+        static void Main(string[] args)
+        {
+            //Swap Check
+            bool swapTest = Swap_Test();
+            Console.WriteLine("Swap test finished with result: \t\t{0}", swapTest);
+            Console.WriteLine("\n");
+
+            //BubbleSort check
+            bool bubbleSortTest = BubbleSort_Test();
+            Console.WriteLine("BubbleSort test finished with result: \t\t{0}", bubbleSortTest);
+            Console.WriteLine("\n");
+
+            //ShakerSort check
+            bool shakerSortTest = ShakerSort_Test();
+            Console.WriteLine("ShakerSort test finished with result: \t\t{0}", shakerSortTest);
+            Console.WriteLine("\n");
+
+            //ShakerSort check
+            bool insertionSortTest = InsertionSort_Test();
+            Console.WriteLine("InsertionSort test finished with result: \t{0}", insertionSortTest);
+            Console.WriteLine("\n");
+
+
+            Console.Read();
+            Console.Read();
+
+        }
+
+        public static bool Swap_Test()
+        {
+            int a = 5;
+            int b = 7;
+            int old_a = a;
+            int old_b = b;
+
+            Sorting.Swap(ref a, ref b);
+            
+            if (old_a == b && old_b == a)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public static bool BubbleSort_Test()
+        {
+            Random rand = new Random();
+            int[] array = new int[MAXN];
+
+            for (int i = 0; i < MAXN; i++)
+            {
+                array[i] = rand.Next(RANDMIN, RANDMAX);
+                //Uncomment this to see UNSORTED ARRAY
+                //Console.Write("{0} ", array[i]);
+            }
+
+            Sorting.BubbleSort(ref array);
+
+            for (int i = 0; i < MAXN-1; i++)
+            {
+                //Uncomment this to see SORTED ARRAY
+                //Console.Write("{0} ", array[i]);
+                if (array[i] > array[i+1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool ShakerSort_Test()
+        {
+            Random rand = new Random();
+            int[] array = new int[MAXN];
+
+            for (int i = 0; i < MAXN; i++)
+            {
+                array[i] = rand.Next(RANDMIN, RANDMAX);
+                //Uncomment this to see UNSORTED ARRAY
+                //Console.Write("{0} ", array[i]);
+            }
+
+            Sorting.ShakerSort(ref array);
+
+            for (int i = 0; i < MAXN - 1; i++)
+            {
+                //Uncomment this to see SORTED ARRAY
+                //Console.Write("{0} ", array[i]);
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool InsertionSort_Test()
+        {
+            Random rand = new Random();
+            int[] array = new int[MAXN];
+
+            for (int i = 0; i < MAXN; i++)
+            { 
+                array[i] = rand.Next(RANDMIN, RANDMAX);
+                //Uncomment this to see UNSORTED ARRAY
+                //Console.Write("{0} ", array[i]);
+            }
+
+            //Console.WriteLine("\n");
+            Sorting.InsertionSort(ref array);
+
+            for (int i = 0; i < MAXN - 1; i++)
+            {
+                //Uncomment this to see SORTED ARRAY
+                //Console.Write("{0} ", array[i]);
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+            //Console.WriteLine("\n");
+            return true;
+        }
+
+
+
+    }
+}
---
+++ Sorts/Sorting.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    class Sorting
+    {
+        //Swap without temp
+        public static void Swap(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        }
+
+        //Bubble Sort                   [https://en.wikipedia.org/wiki/Bubble_sort]
+        //Worst performance:            O(n^2)
+        //Best performance:             O(n)
+        //Avarage performance:          O(n^2)
+        //Worst case space complexity:  O(1)
+        public static void BubbleSort(ref int[] array)
+        {
+            int N = array.Length;
+            for (int i = 0; i < N-1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < N - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapped = true;
+                    }
+                }
+                if(!swapped)
+                {
+                    break;
+                }
+            }
+        }
+
+
+        //Shaker Sort                   [https://en.wikipedia.org/wiki/Cocktail_shaker_sort]
+        //Worst performance:            O(n^2)
+        //best performance:             O(n)
+        //Avarage performance:          O(n^2)
+        //Worst case space complexity:  O(1)
+        public static void ShakerSort(ref int[] array)
+        {
+            int left = 0,
+                right = array.Length - 1;
+
+            while (left <= right)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+                    }
+                }
+                right--;
+
+                for (int i = right; i > left; i--)
+                {
+                    if (array[i-1] > array[i])
+                    {
+                        Swap(ref array[i-1], ref array[i]);
+                    }
+                }
+                left++;
+            }
+
+
+        }
+
+
+        //Insertion sort                [https://en.wikipedia.org/wiki/Insertion_sort]
+        //Worst performance:            О(n^2) comparisons, swaps
+        //Best performance:             O(n) comparisons, O(1) swaps
+        //Avarage performance:          О(n^2) comparisons, swaps
+        //Worst case space complexity:  О(n) total, O(1) auxiliary
+        public static void InsertionSort(ref int[] array)
+        {
+            int N = array.Length;
+
+            for (int i = 1; i < N; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+        }
+
+
+        //Merge sort                    [https://en.wikipedia.org/wiki/Merge_sort]
+        //Worst performance:            О(n log n)
+        //Best performance:             O(n log n) typical, O(n) natural variant
+        //Avarage performance:          O(n log n)
+        //Worst case space complexity:  О(n) total, O(n) auxiliary
+        public static void MergeSort(ref int[] array)
+        {
+
+        }
+
+        //TODO: Quick Sort Description
+        public static void QuickSort()
+        {
+            //TODO: Quick Sort
+        }
+
+    }
+}
